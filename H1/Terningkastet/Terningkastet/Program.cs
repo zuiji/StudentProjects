3e1d8f076e6eee58abf45fd37f37895899708012
Peter Bøgh Stubberup <pete646a@zbc.dk>
2019-01-24 11:50:36 +0100

    Done

--- H1/Terningkastet/Terningkastet/Program.cs
+++ H1/Terningkastet/Terningkastet/Program.cs
@@ -10,12 +10,44 @@ namespace Terningkastet
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Random random = new Random();
+            int terningskast = random.Next(1, 7);
+
+            if (terningskast == 1)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Du slog en etter");
+            }
+            else if (terningskast == 2)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Du slog en to'er");
+            }
+            else if (terningskast == 3)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Du slog en tre'er");
+            }
+            else if (terningskast == 4)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Du slog en fire'er");
+            }
+            else if (terningskast == 5)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Du slog en fem'er");
+            }
+            else if (terningskast == 6)
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("Do slog en seks'er");
+            }
+
+            Console.ReadKey();
         }
     }
 }
