683bab759e652586b51d09d3c7996607407a6896
Peter Bøgh Stubberup <pete646a@zbc.dk>
2019-01-24 10:59:07 +0100

    Jobs done

--- H1/Rumfanget/Rumfanget/Program.cs
+++ H1/Rumfanget/Rumfanget/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,12 +11,18 @@ namespace Rumfanget
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Console.WriteLine("Intast højden på din kasse");
+            double Hight = double.Parse(Console.ReadLine());
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            Console.WriteLine("Intast breden på din kasse");
+            double Wirght = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("indtast længten af din kasse.");
+            double Lenth = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Rumfanget af din kasse er : "+  Hight * Wirght * Lenth );
+
+            Console.ReadKey();
         }
     }
 }
