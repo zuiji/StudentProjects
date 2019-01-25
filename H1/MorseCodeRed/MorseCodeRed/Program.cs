d05e7885e862839acb9771c32a66e7d0641d310d
Peter Bøgh Stubberup <peter.stubberup@live.dk>
2019-01-25 09:38:31 +0100

    CodeClean up and Camelletters

--- H1/MorseCodeRed/MorseCodeRed/Program.cs
+++ H1/MorseCodeRed/MorseCodeRed/Program.cs
@@ -10,12 +10,61 @@ namespace MorseCodeRed
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            #region dictionary
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app!    
+            Dictionary<char, string> morsetranslater = new Dictionary<char, string>();
+            morsetranslater.Add('A', ".-");
+            morsetranslater.Add('B', "-...");
+            morsetranslater.Add('C', "-.-.");
+            morsetranslater.Add('D', "-..");
+            morsetranslater.Add('E', ".");
+            morsetranslater.Add('F', "..-");
+            morsetranslater.Add('G', "--.");
+            morsetranslater.Add('H', "....");
+            morsetranslater.Add('I', "..");
+            morsetranslater.Add('J', ".---");
+            morsetranslater.Add('K', "-.-");
+            morsetranslater.Add('L', ".-..");
+            morsetranslater.Add('M', "--");
+            morsetranslater.Add('N', "-.");
+            morsetranslater.Add('O', "---");
+            morsetranslater.Add('P', ".--.");
+            morsetranslater.Add('Q', "--.-");
+            morsetranslater.Add('R', ".-.");
+            morsetranslater.Add('S', "...");
+            morsetranslater.Add('T', "-");
+            morsetranslater.Add('U', "..-");
+            morsetranslater.Add('V', "...-");
+            morsetranslater.Add('W', ".--");
+            morsetranslater.Add('X', "-..-");
+            morsetranslater.Add('Y', "-.--");
+            morsetranslater.Add('Z', "--..");
+            morsetranslater.Add('Æ', ".-.-");
+            morsetranslater.Add('Ø', "---.");
+            morsetranslater.Add('Å', ".--.-");
+
+
+
+            #endregion
+
+            Console.WriteLine("Enter the text you want to translate to morse code");
+            string inputFromUser = Console.ReadLine().ToUpper();
+            char[] charArray = inputFromUser.ToCharArray();
+            StringBuilder morse = new StringBuilder();
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                if (!morsetranslater.ContainsKey(charArray[i]))
+                {
+                    morse.Append(" ");
+                    continue;
+                }
+                
+                morse.Append(morsetranslater[charArray[i]]);
+                
+            }
+
+            Console.WriteLine(morse);
+
         }
     }
-}
+} 
