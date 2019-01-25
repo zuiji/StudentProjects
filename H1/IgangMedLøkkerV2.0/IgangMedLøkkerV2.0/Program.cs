c7f92f6b31e39a82556c0cb4d2d2086496a7ded5
Peter Bøgh Stubberup <peter.stubberup@live.dk>
2019-01-25 12:35:35 +0100

    C done.

---
+++ H1/IgangMedLøkkerV2.0/IgangMedLøkkerV2.0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IgangMedLøkkerV2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i <= 100)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
