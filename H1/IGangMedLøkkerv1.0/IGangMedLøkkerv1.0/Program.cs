0e2225911c9ace70b74209fca1ee8bbaac6a6c12
Peter Bøgh Stubberup <peter.stubberup@live.dk>
2019-01-25 11:57:10 +0100

    A + B done.

---
+++ H1/IGangMedLøkkerv1.0/IGangMedLøkkerv1.0/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IGangMedLøkkerv1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+
+                if (i < 50)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+}
