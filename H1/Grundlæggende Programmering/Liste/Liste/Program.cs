eb862dafdf370b3d3e6192d34d5f7e88b799a70b
Peter Bøgh Stubberup <peter.stubberup@live.dk>
2019-01-28 19:47:37 +0100

    Temp Stuck

--- H1/Grundlæggende Programmering/Liste/Liste/Program.cs
+++ H1/Grundlæggende Programmering/Liste/Liste/Program.cs
@@ -1,21 +1,18 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Liste
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            List<int> numbers = new List<int>();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            for (int i = 0; i < 20; i++) numbers.Add(i);
+
+
+            Console.WriteLine(numbers);
         }
     }
 }
