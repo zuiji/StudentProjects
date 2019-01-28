0d70b7a69e3d3c1cb47dd409d3c8805a6dd48d2e
Peter Bøgh Stubberup <peter.stubberup@live.dk>
2019-01-28 20:26:00 +0100

    kan ikke få den til at kører

--- H1/Grundlæggende Programmering/Liste/Liste/Program.cs
+++ H1/Grundlæggende Programmering/Liste/Liste/Program.cs
@@ -1,18 +1,28 @@
+
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Liste
 {
     class Program
     {
-        private static void Main(string[] args)
+        static void Main(string[] args)
         {
             List<int> numbers = new List<int>();
 
-            for (int i = 0; i < 20; i++) numbers.Add(i);
+            for (int i = 0; i <= 20; i++) 
+            {
+                numbers.Add(i);
+                Console.WriteLine(numbers);
+
+            }
 
 
-            Console.WriteLine(numbers);
+            
         }
     }
 }
