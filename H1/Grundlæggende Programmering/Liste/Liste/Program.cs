00e9ce55ffe1e7231710f67636d38b8da50a3ed8
Peter Bøgh Stubberup <peter.stubberup@live.dk>
2019-01-28 20:46:43 +0100

    dada

--- H1/Grundlæggende Programmering/Liste/Liste/Program.cs
+++ H1/Grundlæggende Programmering/Liste/Liste/Program.cs
@@ -1,18 +1,27 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
 
 namespace Liste
 {
     class Program
     {
-        private static void Main(string[] args)
+        static void Main(string[] args)
         {
             List<int> numbers = new List<int>();
 
-            for (int i = 0; i < 20; i++) numbers.Add(i);
-
-
-            Console.WriteLine(numbers);
+            for (int i = 0; i < 20; i++)
+            {
+                numbers.Add(i);
+                Console.WriteLine(numbers);
+            }
         }
     }
 }
