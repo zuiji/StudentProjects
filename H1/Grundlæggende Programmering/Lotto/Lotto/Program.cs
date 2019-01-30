85601527e3e3273797cfd1978e6c1a49aead37e5
Peter Bøgh Stubberup <peter.stubberup@live.dk>
2019-01-30 18:26:37 +0100

    code Clean up

--- H1/Grundlæggende Programmering/Lotto/Lotto/Program.cs
+++ H1/Grundlæggende Programmering/Lotto/Lotto/Program.cs
@@ -1,15 +1,10 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Principal;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lotto
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             int[] lottoArray = new int[7];
             int[] kuponArray = new int[7];
@@ -19,43 +14,42 @@ namespace Lotto
             {
                 int ran = r.Next(1, 20);
                 lottoArray[i] = ran;
-                //Console.WriteLine(ran);
+
             }
 
             for (int i = 0; i < lottoArray.Length; i++)
             {
-                for (int j = 0; j < (lottoArray.Length - 1); j++)
+                for (int j = 0; j < lottoArray.Length - 1; j++)
                 {
                     if (lottoArray[j] < lottoArray[j + 1])
-                        continue;
-                    else
                     {
-                        int temp = lottoArray[j];
-                        lottoArray[j] = lottoArray[j + 1];
-                        lottoArray[j + 1] = temp;
-
+                        continue;
                     }
+
+                    int temp = lottoArray[j];
+                    lottoArray[j] = lottoArray[j + 1];
+                    lottoArray[j + 1] = temp;
                 }
             }
+
             for (int i = 0; i < kuponArray.Length; i++)
             {
                 int ran = r.Next(1, 20);
                 kuponArray[i] = ran;
-                //Console.WriteLine(ran);
+
             }
 
             for (int i = 0; i < kuponArray.Length; i++)
             {
-                for (int j = 0; j < (kuponArray.Length - 1); j++)
+                for (int j = 0; j < kuponArray.Length - 1; j++)
                 {
                     if (kuponArray[j] < kuponArray[j + 1])
-                        continue;
+                    { }
                     else
                     {
                         int temp1 = kuponArray[j];
                         kuponArray[j] = kuponArray[j + 1];
                         kuponArray[j + 1] = temp1;
-
                     }
                 }
             }
@@ -63,7 +57,7 @@ namespace Lotto
             Console.WriteLine("The lotto numbers that was drawn is:\n ");
             foreach (int int32 in lottoArray)
             {
-                System.Console.WriteLine(int32);
+                Console.WriteLine(int32);
             }
 
             Console.WriteLine();
@@ -71,10 +65,10 @@ namespace Lotto
             Console.WriteLine("Your kupon numbers is:\n ");
             foreach (int int32 in kuponArray)
             {
-                System.Console.WriteLine(int32);
+                Console.WriteLine(int32);
             }
 
-            int correctNumbers= 0;
+            int correctNumbers = 0;
             foreach (int k in kuponArray)
             {
                 foreach (int l in lottoArray)
@@ -109,7 +103,6 @@ namespace Lotto
                 default:
                     Console.WriteLine("To bad you did not win anything, Try again next week.");
                     break;
-             
             }
 
             if (prize > 0)
