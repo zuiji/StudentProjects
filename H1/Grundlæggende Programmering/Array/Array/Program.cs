b5273a039aba90b8d406585ef2facd4a1a198a4e
Peter Bøgh Stubberup <peter.stubberup@live.dk>
2019-02-11 22:40:52 +0100

    Trying to rework it so i maby can get it to work

--- H1/Grundlæggende Programmering/Array/Array/Program.cs
+++ H1/Grundlæggende Programmering/Array/Array/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,75 +12,68 @@ namespace Array
     {
         static void Main(string[] args)
         {
+
+            Opg1();
+        }
+
+        private static void Opg1()
+        {
             #region Øvelse 1: login
 
             //arrays containing usernames and passwords, the index space in usernames corresponds to the index space in password
             string[] userNameArrays = new string[5] { "user1", "user2", "user3", "User4", "user5" };
             string[] passwordArrays = new string[5] { "pass1", "pass2", "pass3", "pass4", "pass5" };
 
-
-            bool userPass = false;
-
-            int failPassword;
-            int failUsername;
-
-            bool login = true;
-
-            while (login == true)
+            bool answers = true;
+            do
             {
                 Console.WriteLine("Enter your UserName");
                 string username = Console.ReadLine();
-
-
 
                 for (int i = 0; i < userNameArrays.Length; i++)
                 {
                     if (username == userNameArrays[i])
                     {
                         Console.WriteLine("Enter your Password");
-                        string password = Console.ReadLine();
 
-                        for (int count = 3; count > 0; count--)
-                        {
-                            if (password != passwordArrays[i])
-                            {
-
-                                Console.WriteLine("Username or Password don't match");
-                            }
-                            if (password == passwordArrays[i])
-                            {
-                                Console.WriteLine("you are logged in.");
-                                count = 0;
-                            }
-                        }
-
-                        while (userPass != false)
-                        {
-                            for (int count = 3; count > 0; count--)
-                            {
-                                if (password != passwordArrays[i])
-                                {
-
-                                    Console.WriteLine("Username or Password don't match");
-                                }
-                                if (password == passwordArrays[i])
-                                {
-                                    Console.WriteLine("you are logged in.");
-                                    count = 0;
-                                }
-                            }
-
-                        }
                     }
+                }
+                string password = Console.ReadLine();
 
-
+                for (int j = 0; j < passwordArrays.Length; j++)
+                {
+                    if (password != passwordArrays[j]) ;
+                    
                 }
 
+                foreach (string k in userNameArrays)
+                {
+                    foreach (string l in passwordArrays)
+                    {
+                        if (k == l)
+                        {
+                            Console.WriteLine("You have logged in");
 
-            }
+                        }
+                        /*else if (k != l)
+                        {
 
+                            Console.WriteLine("Try again.");
 
+                        }*/
+                        else
+                        {
+                            Console.WriteLine("Your Username or Password are not Correct");
+                        }
+
+                    }
+
+                }
+                
+            } while (true);
             #endregion
         }
     }
+
+
 }
