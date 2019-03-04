c26c6e6d64fe5de1166f12b4e2bbe317defaa1ff
Peter Bøgh Stubberup <pete646a@zbc.dk>
2019-03-04 09:46:16 +0100

    Splittet out in 3 lags

---
+++ H1/Modul1/Miljø AD tool/Miljø AD tool/GetUserInfo.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miljø_AD_tool
+{
+    class GetUserInfo
+    {
+        public static string UserInfo(string userName, DirectoryEntry CreateDirectoryEntry)
+        {
+
+            try
+            {
+                // create LDAP connection object  
+
+                DirectoryEntry myLdapConnection = CreateDirectoryEntry;
+
+
+                // create search object which operates on LDAP connection object  
+                // and set search object to only find the user specified  
+
+                DirectorySearcher search = new DirectorySearcher(myLdapConnection);
+                //  search.Filter = $"(sAMAccountName= {username})";
+                // create results objects from search object  
+
+                SearchResult result = search.FindOne();
+                if (result != null)
+                {
+                    // user exists, cycle through LDAP fields (cn, telephone number etc.)  
+
+                    ResultPropertyCollection fields = result.Properties;
+
+                    foreach (string ldapField in fields.PropertyNames)
+                    {
+                        // cycle through objects in each field e.g. group membership  
+                        // (for many fields there will only be one object such as name)  
+
+                        foreach (Object myCollection in fields[ldapField])
+                            Console.WriteLine(string.Format("{0,-20} : {1}",
+                                                            ldapField, myCollection.ToString()));
+                    }
+                }
+
+                else
+                {
+                    // user does not exist  
+                    return "User not found!";
+                }
+            }
+
+            catch (Exception e)
+            {
+                return ("Exception caught:\n\n" + e.ToString());
+            }
+
+            return null;
+        }
+
+       
+    }
+}
--- H1/Modul1/Miljø AD tool/Miljø AD tool/Program.cs
+++ H1/Modul1/Miljø AD tool/Miljø AD tool/Program.cs
@@ -12,134 +12,39 @@ namespace Miljø_AD_tool
     {
         static void Main(string[] args)
         {
+            ConnectionPath.CreateDirectoryEntry();
 
-            Console.WriteLine("Press 1 to read userinfo :\nPress 2 to addUser :\nPress any other buttons to stop : ");
+            Console.WriteLine("Press 1 to read userinfo :\nPress 2 to addUser :\nPress 9 to stop : ");
             char inputFromUser = Console.ReadKey(true).KeyChar;
             switch (inputFromUser)
             {
                 case '1':
-                    UserInfo();
+
+                    Console.Write("Enter user: ");
+                    string username = Console.ReadLine();
+                    GetUserInfo.UserInfo(username, ConnectionPath.CreateDirectoryEntry());
                     break;
 
                 case '2':
-                    CreateUserAccount();
+                    // Asking for Users First name and last name 
+                    Console.WriteLine("Please enter Firstname");
+                    string firstName = Console.ReadLine();
+                    Console.WriteLine("Please enter LastName");
+                    string lastname = Console.ReadLine();
+
+                    // Calls method and sending variables.
+                    CreateUser.CreateUserAccount(firstName, lastname, ConnectionPath.CreateDirectoryEntry());
+                    break;
+
+                case '9':
+                    Console.WriteLine("To bad to see you go.\nHave a nice day");
+                    Environment.Exit(0);
                     break;
 
                 default:
+                    Console.WriteLine("Wrong KeyChar try with 1 - 3, Please :') ");
                     break;
             }
         }
-
-        public static void UserInfo()
-        {
-            Console.Write("Enter user: ");
-            string username = Console.ReadLine();
-            try
-            {
-                // create LDAP connection object  
-
-                DirectoryEntry myLdapConnection = CreateDirectoryEntry();
-
-
-                // create search object which operates on LDAP connection object  
-                // and set search object to only find the user specified  
-
-                DirectorySearcher search = new DirectorySearcher(myLdapConnection);
-                //  search.Filter = $"(sAMAccountName= {username})";
-                // create results objects from search object  
-
-                SearchResult result = search.FindOne();
-                if (result != null)
-                {
-                    // user exists, cycle through LDAP fields (cn, telephone number etc.)  
-
-                    ResultPropertyCollection fields = result.Properties;
-
-                    foreach (string ldapField in fields.PropertyNames)
-                    {
-                        // cycle through objects in each field e.g. group membership  
-                        // (for many fields there will only be one object such as name)  
-
-                        foreach (Object myCollection in fields[ldapField])
-                            Console.WriteLine(string.Format("{0,-20} : {1}",
-                                          ldapField, myCollection.ToString()));
-                    }
-                }
-
-                else
-                {
-                    // user does not exist  
-                    Console.WriteLine("User not found!");
-                }
-            }
-
-            catch (Exception e)
-            {
-                Console.WriteLine("Exception caught:\n\n" + e.ToString());
-            }
-        }
-
-        static DirectoryEntry CreateDirectoryEntry()
-        {
-            // create and return new LDAP connection with desired settings  
-
-            DirectoryEntry ldapConnection = new DirectoryEntry("LDAP://192.168.20.2", "Administrator", "jmp1234!");
-            ldapConnection.AuthenticationType = AuthenticationTypes.Secure;
-
-            return ldapConnection;
-        }
-        public static void CreateUserAccount()
-        {
-            try
-            {
-                // Asking for Users First name and last name 
-                Console.WriteLine("Please enter Firstname");
-                string firstName = Console.ReadLine();
-                Console.WriteLine("Please enter LastName");
-                string lastname = Console.ReadLine();
-
-                // contains Standard Password. 
-                string password = "User1234!";
-
-                // adding a X if Users first name or last name is under 3 chars long. and its added to the right of his name.
-                string loginName = firstName.PadRight(3, 'X').Substring(0, 3) + lastname.PadRight(3, 'X').Substring(0, 3);
-
-                // collects firstname and lastname
-                string fullname = $"{firstName} {lastname}";
-
-                // Using LDAP connection object
-                DirectoryEntry dirEntry = CreateDirectoryEntry();
-
-                // Adding a Child ( new User ) with fullname with are a collection of FirstName and LastName
-                DirectoryEntry newUser = dirEntry.Children.Add($"CN={ fullname}", "user");
-                newUser.Properties["samAccountName"].Value = loginName;
-                newUser.Properties["sn"].Value = lastname;
-                newUser.Properties["givenName"].Value = firstName;
-                newUser.Properties["userPrincipalName"].Value = loginName + "@H1.com";
-
-                // Saves the Changes
-                newUser.CommitChanges();
-
-                //If you dont have an SSL connection you can not set password
-                newUser.Invoke("SetPassword", new object[] { password });
-                newUser.Properties["LockOutTime"].Value = 0;
-                //Enable user
-                int val = (int)newUser.Properties["userAccountControl"].Value;
-                newUser.Properties["userAccountControl"].Value = val & ~0x2;
-                // Saves The changes with Password
-                newUser.CommitChanges();
-
-                // Closing connection
-                dirEntry.Close();
-
-                // Closing connection
-                newUser.Close();
-            }
-            catch (System.DirectoryServices.DirectoryServicesCOMException E)
-            {
-                Console.WriteLine(E);
-            }
-        }
-
     }
 }
