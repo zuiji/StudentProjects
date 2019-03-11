b0ffd8a6a119cfd34d56e4e3a5ef08e6e38facda
Peter Bøgh Stubberup <pete646a@zbc.dk>
2019-03-11 13:32:22 +0100

    done with primeNumbers

--- H1/Object Programmering/playWithPrimes/playWithPrimes/Program.cs
+++ H1/Object Programmering/playWithPrimes/playWithPrimes/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace playWithPrimes
 {
@@ -10,6 +7,33 @@ namespace playWithPrimes
     {
         static void Main(string[] args)
         {
+            Queue<int> PrimeNumbers = new Queue<int>();
+            PrimeNumbers.Enqueue(2);
+            PrimeNumbers.Enqueue(3);
+            PrimeNumbers.Enqueue(5);
+            PrimeNumbers.Enqueue(7);
+            PrimeNumbers.Enqueue(11);
+
+            int total = 0;
+
+            // Running each Queue truth the Queue PrimeNumber
+            foreach (int number in PrimeNumbers)
+            {
+                Console.WriteLine($"primeNumber: {number}");
+            }
+
+            //new line for better look out
+            Console.WriteLine("");
+
+            // counting all the primes total there is in Queue and removing them from the Queue list. 
+            while (PrimeNumbers.Count != 0)
+            {
+                total += PrimeNumbers.Dequeue();
+            }
+            // prints out the total amount of the Queue
+            Console.WriteLine("total: " + total);
+
+
         }
     }
 }
