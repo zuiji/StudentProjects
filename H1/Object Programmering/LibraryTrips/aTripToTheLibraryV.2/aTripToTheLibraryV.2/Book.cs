e4dc13879f874b9e1acade19b522bd4b82522d47
Peter Bøgh Stubberup <pete646a@zbc.dk>
2019-03-12 13:28:47 +0100

    Starting on a Trip on libery v2

---
+++ H1/Object Programmering/LibraryTrips/aTripToTheLibraryV.2/aTripToTheLibraryV.2/Book.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aTripToTheLibraryV._2
+{
+    class Book
+    {
+        private string author;
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+
+        private string title;
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        private string genre;
+        public string Genre
+        {
+            get { return genre; }
+            set { genre = value; }
+        }
+
+        private int pages;
+        public int Pages
+        {
+            get { return pages; }
+            set { pages = value; }
+        }
+
+        public Book(string author, string title, string genre, int pages)
+        {
+            Author = author;
+            Title = title;
+            Genre = genre;
+            Pages = pages;
+
+        }
+    }
+}
