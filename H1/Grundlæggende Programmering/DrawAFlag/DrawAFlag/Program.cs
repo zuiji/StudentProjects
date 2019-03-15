f0c8edcf32e5d61204cd291372037cda8c2f9ab7
Peter Bøgh Stubberup <pete646a@zbc.dk>
2019-03-15 08:55:12 +0100

    done with drawAFlag

---
+++ H1/Grundlæggende Programmering/DrawAFlag/DrawAFlag/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawAFlag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("* * * * * * ");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Write("                                      \n");
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(" * * * * *  ");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Write("                                      \n");
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("* * * * * * ");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Write("                                      \n");
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(" * * * * *  ");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Write("                                      \n");
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("* * * * * * ");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Write("                                      \n");
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(" * * * * *  ");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Write("                                      \n");
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("* * * * * * ");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Write("                                      \n");
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(" * * * * *  ");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Write("                                      \n");
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("* * * * * * ");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Write("                                      \n");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("                                                  ");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("                                                  ");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("                                                  ");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("                                                  ");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("                                                  ");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("                                                  ");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine("");
+        }
+    }
+}
