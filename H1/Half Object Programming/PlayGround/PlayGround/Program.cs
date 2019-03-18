4a4a2ed09a688477278b83dc2d4b420b4e9452ab
Peter Bøgh Stubberup <peter.stubberup@live.dk>
2019-03-18 18:59:38 +0100

    Husk at læse Comments under Phones

---
+++ H1/Half Object Programming/PlayGround/PlayGround/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayGround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PhoneViewModel VM = new PhoneViewModel();
+            Console.WriteLine(VM.PrintAll());
+
+
+        }
+
+    }
+}
