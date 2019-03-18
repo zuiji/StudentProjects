1dc29f16bc71ddd13f76a02e417e3784e19b20f5
Peter Bøgh Stubberup <pete646a@zbc.dk>
2019-03-18 13:22:40 +0100

    ush

---
+++ H1/Half Object Programming/Car/Car/Car.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car
+{
+    public class Car
+    {
+        private string color;
+        private int weel;
+        private int door;
+        private int year;
+        private string type;
+
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+
+        public int Weel
+        {
+            get { return weel; }
+            set { weel = value; }
+        }
+
+
+        public int Door
+        {
+            get { return door; }
+            set { door = value; }
+        }
+
+
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+
+        public string Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+
+        public Car()
+        {
+            List.add(this>myCar)
+        }
+
+
+        public Car(string color, int weel, int door, int year, string type)
+        {
+            color = Color;
+            weel = Weel;
+            door = Door;
+            year = Year;
+            type = Type;
+            //list.add(this>)
+        }
+
+        public List<Car> myList = new List<Car>();
+        
+    }
+
+}
---
+++ H1/Half Object Programming/Car/Car/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Console.WriteLine("");
+
+
+        }
+
+        public void AddObject()
+        {
+            //Object 
+            Car myCar = new Car();
+            //object info
+            myCar.Color = "Red";
+
+            //Object
+            Car b = new Car();
+            //object info
+            b.Type = "VW";
+
+            //object
+            Car c = new Car();
+
+            //object info
+            c.Door = 5;
+            c.Weel = 4;
+            c.Year = 2008;
+        }
+    }
+}
