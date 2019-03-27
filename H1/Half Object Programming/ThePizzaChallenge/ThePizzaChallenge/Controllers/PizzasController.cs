fed530f6d5430a09f907585c343a22b7f9fdb567
Peter Bøgh Stubberup <peter.stubberup@live.dk>
2019-03-27 19:00:26 +0100

    PizzaChangelse

---
+++ H1/Half Object Programming/ThePizzaChallenge/ThePizzaChallenge/Controllers/PizzasController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ThePizzaChallenge.Data;
+using ThePizzaChallenge.Models;
+
+namespace ThePizzaChallenge.Controllers
+{
+    public class PizzasController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PizzasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Pizzas
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Pizzas.ToListAsync());
+        }
+
+        // GET: Pizzas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pizza = await _context.Pizzas
+                .FirstOrDefaultAsync(m => m.PizzaId == id);
+            if (pizza == null)
+            {
+                return NotFound();
+            }
+            return View(pizza);
+        }
+
+        // GET: Pizzas/Create
+        public async Task<IActionResult> Create()
+        {
+           var cp = new CreatePizzaViewModel();
+            cp.Pizza = new Pizza();
+            cp.AllIngredients = await _context.Ingredientses.ToListAsync();
+            return View(cp);
+        }
+
+        // POST: Pizzas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CreatePizzaViewModel createPizzaViewModel)
+        {
+            createPizzaViewModel.Pizza.Ingredients = new Collection<Ingredients>();
+            foreach (Ingredients ingredientse in createPizzaViewModel.AllIngredients.Where(i => i.IsSelected))
+            {
+                createPizzaViewModel.Pizza.Ingredients.Add(ingredientse);
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Add(createPizzaViewModel.Pizza);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(createPizzaViewModel);
+        }
+
+        // GET: Pizzas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pizza = await _context.Pizzas.FindAsync(id);
+            if (pizza == null)
+            {
+                return NotFound();
+            }
+            return View(pizza);
+        }
+
+        // POST: Pizzas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PizzaId,PizzaName")] Pizza pizza)
+        {
+            if (id != pizza.PizzaId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(pizza);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PizzaExists(pizza.PizzaId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(pizza);
+        }
+
+        // GET: Pizzas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pizza = await _context.Pizzas
+                .FirstOrDefaultAsync(m => m.PizzaId == id);
+            if (pizza == null)
+            {
+                return NotFound();
+            }
+
+            return View(pizza);
+        }
+
+        // POST: Pizzas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var pizza = await _context.Pizzas.FindAsync(id);
+            _context.Pizzas.Remove(pizza);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PizzaExists(int id)
+        {
+            return _context.Pizzas.Any(e => e.PizzaId == id);
+        }
+    }
+}
---
+++ H1/Half Object Programming/ThePizzaChallenge/ThePizzaChallenge/Models/Pizza.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThePizzaChallenge.Models
+{
+    public class Pizza
+    {
+        [Key]
+        public int PizzaId {get; set;}
+
+        public string PizzaName {get; set;}
+        public Pizza()
+        {
+        }
+
+        public ICollection<Ingredients> Ingredients {get; set;}
+
+        public decimal Price()
+        {
+            decimal teDecimal=0;
+            foreach (Ingredients ingredientse in Ingredients)
+            {
+               teDecimal += ingredientse.PriceDecimal;
+            }
+
+            return teDecimal;
+        }
+
+    }
+
+    public class Ingredients
+    {
+        [Key]
+        public int IngredientsId {get; set;}
+
+        public string IngredientsName { get; set;}
+
+        public decimal PriceDecimal {get; set;}
+        [NotMapped]
+        public bool IsSelected {get; set;}
+
+        public Ingredients()
+        {
+            
+        }
+        public Ingredients(string ingredientsName, decimal priceDecimal)
+        {
+            this.IngredientsName = ingredientsName;
+            PriceDecimal = priceDecimal;
+        }
+    }
+}
