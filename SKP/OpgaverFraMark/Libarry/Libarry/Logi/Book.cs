cd8bf2d0998c882db8229f9b751fb1482a43af71
Peter Bøgh Stubberup <pete646a@zbc.dk>
2019-04-08 11:12:59 +0200

    renamed BookClass to Book

---
+++ SKP/OpgaverFraMark/Libarry/Libarry/Logi/Book.cs
@@ -0,0 +1,21 @@
+﻿namespace Libarry
+{
+    class Book
+    {
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+
+        public Book(string author, string title, string genre)
+        {
+            Author = author;
+            Title = title;
+            Genre = genre;
+        }
+
+        public Book()
+        {
+
+        }
+    }
+}
