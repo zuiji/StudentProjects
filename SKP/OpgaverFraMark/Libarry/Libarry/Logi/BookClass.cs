7c8b3ba55d7473bc9798532306938dc3cfcd2ebb
Peter Bøgh Stubberup <pete646a@zbc.dk>
2019-04-08 10:49:56 +0200

    Created Constructor

--- SKP/OpgaverFraMark/Libarry/Libarry/Logi/BookClass.cs
+++ SKP/OpgaverFraMark/Libarry/Libarry/Logi/BookClass.cs
@@ -5,5 +5,18 @@
         public string Author { get; set;}
         public string Title { get; set;}
         public string Genre { get; set;}
+
+        public BookClass(string author, string title, string genre)
+        {
+            author = Author;
+            title = Title;
+            genre = Genre;
+
+        }
+
+        public BookClass()
+        {
+
+        }
     }
 }
