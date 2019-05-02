e51e5604c03c560991d73f44cf14c13ae80b67ca
Peter Bøgh Stubberup <peter.stubberup@live.dk>
2019-05-02 13:50:51 +0200

    Starting up my Quiz Project

---
+++ SKP/OpgaverFraMark/CQuestionFui/CQuestionFui/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CQuestionFui
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
