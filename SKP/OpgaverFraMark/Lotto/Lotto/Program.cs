b16938e3361dec3bcd43b4193c39d57ac6b2e9ab
Peter Bøgh Stubberup <peter.stubberup@live.dk>
2019-05-06 15:03:27 +0200

    Lotto

--- SKP/OpgaverFraMark/Lotto/Lotto/Program.cs
+++ SKP/OpgaverFraMark/Lotto/Lotto/Program.cs
@@ -7,7 +7,7 @@ namespace Lotto
         static void Main(string[] args)
         {
             int[] LottoArray = new int[7];
-            int[] kuponArray = new int[7];
+            int[] CouponArray = new int[7];
             Random random = new Random();
 
             for (int i = 0; i < LottoArray.Length; i++)
@@ -16,9 +16,22 @@ namespace Lotto
                 LottoArray[i] = ran;
             }
 
+            for (int k = 0; k < CouponArray.Length; k++)
+            {
+                int ran = random.Next(1, 48);
+                CouponArray[k] = ran;
+            }
+
+            Console.WriteLine("The lottery numbers: ");
             foreach (int i in LottoArray)
             {
                 Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Your coupon numbers: ");
+            foreach (int k in CouponArray)
+            {
+                Console.WriteLine(k);
             }
         }
     }
