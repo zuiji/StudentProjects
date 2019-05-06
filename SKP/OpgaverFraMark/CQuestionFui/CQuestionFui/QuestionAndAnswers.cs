111b94e15ec7c6a63f8a92f301b5207c0c43273c
Peter Bøgh Stubberup <peter.stubberup@live.dk>
2019-05-06 14:56:22 +0200

    lotto

--- SKP/OpgaverFraMark/CQuestionFui/CQuestionFui/QuestionAndAnswers.cs
+++ SKP/OpgaverFraMark/CQuestionFui/CQuestionFui/QuestionAndAnswers.cs
@@ -6,5 +6,6 @@ namespace CQuestionFui
 {
     class Question
     {
+
     }
 }
---
+++ SKP/OpgaverFraMark/Lotto/Lotto/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lotto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] LottoArray = new int[7];
+            int[] kuponArray = new int[7];
+            Random random = new Random();
+
+            for (int i = 0; i < LottoArray.Length; i++)
+            {
+                int ran = random.Next(1, 48);
+                LottoArray[i] = ran;
+            }
+
+            foreach (int i in LottoArray)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
