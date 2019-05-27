927e71afecdff118be3bea6ade3518bde5e5516a
Peter Bøgh Stubberup <peter.stubberup@live.dk>
2019-05-27 15:46:25 +0200

    just pushed

--- SKP/OpgaverFraMark/Lotto/Lotto/NumberGenerator.cs
+++ SKP/OpgaverFraMark/Lotto/Lotto/NumberGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Lotto
@@ -9,17 +10,18 @@ namespace Lotto
 
         #region GeneratsRandomnumbersToLottoArrayAndSorting
         public static int[] Generator()
-
         {
             Random random = new Random();
             // Generate numbers for LottoArray and Sorting it.
             int[] lottoArray = new int[7];
-
+           
             for (int i = 0; i < lottoArray.Length; i++)
             {
                 int ran = random.Next(1, 48);
-                lottoArray[i] = ran;
-                
+                foreach (int i1 in lottoArray[i])
+                {
+                  
+                }
             }
 
             Array.Sort(lottoArray);
